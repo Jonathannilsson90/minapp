5a195178a76da48f5778dc43fceffa5d21caeb1b
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-05-08 13:29:16 +0200

    Init commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View, Button, Alert } from "react-native";
 
 export default function App() {
+const showAlert = () => {
+  Alert.alert('knappen är tryckt')
+}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>React Native, lektion 1</Text>
+      <Button onPress={showAlert} color="red" style={styles.button} title="Tryck här" />
       <StatusBar style="auto" />
+      
+      <TextInput
+      placeholder="Skriv här..."
+      style={styles.input}
+      />
     </View>
+
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff3",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  text: {
+    color: "blue",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  input: {
+    borderWidth: 1,
+    margin: 10,
+    height: 50,
+    width: 300,
+  }
+
 });
